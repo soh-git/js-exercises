9f3e5bea1774701bc2c94f8c5129bc238726c64a
sm-git <mze.souhaib@gmail.com>
2022-10-13 13:36:12 +0200

    up

---
+++ part1/part1.js
@@ -0,0 +1,31 @@
+function revert(str){}
+
+function ucFirst (word){}
+
+function capitalize (str){}
+function pascalCase (str){}
+
+function palindrome(str) {}
+
+
+function findLongestWord(str) {}
+function leet(str) {}
+
+function prop_access(object, path) {}
+
+function type_check_v1(data, type) {}
+
+function type_check_v2(val, condition) {}
+
+module.exports = {
+  revert,
+  ucFirst,
+  capitalize,
+  pascalCase,
+  palindrome,
+  leet,
+  findLongestWord,
+  prop_access,
+  type_check_v1,
+  type_check_v2,
+}
---
+++ part2/part2.js
@@ -0,0 +1,37 @@
+class Mapi  {}
+
+const getHashTags = () => null;
+const removeDuplicate = () => null;
+const intersection = () => null;
+const arrayDiff = () => null;
+
+const combinations = () => null;
+
+const fiscalCode = () => null;
+
+const lcm = () => null;
+
+const merge = () => null;
+const students = [ { name: "Dupont", cours: [ 1, 3, 5 ] }, { name: "Lea", cours: [ 2, 4, 11] }, { name: "Charles", cours: [1] } ];
+const courses = [{id: 1, name: "JS" }, { id: 2, name: "PHP" }, { id: 3, name: "C#" }, { id: 4, name: "F#" }, { id: 5, name: "CSS" }];
+
+const getStudents = () => null;
+
+const getCourses = () => null;
+
+const getStudentsCourses = () => null;
+
+module.exports = {
+  getHashTags,
+  removeDuplicate,
+  intersection,
+  arrayDiff,
+  combinations,
+  fiscalCode,
+  lcm,
+  merge,
+  getStudents,
+  getCourses,
+  getStudentsCourses,
+  Mapi,
+}
